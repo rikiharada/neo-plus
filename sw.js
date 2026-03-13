79a163e2dc68a5421b182d793b5cab176d995bd0
Neo+ Antigravity <ceo@neo-plus.app>
2026-03-13 22:31:44 +0900

    fix(ui): restore missing closing div and bump version for cache busting

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 console.log('[SW] Service Worker is now a designated DESTROYER.');
 
 self.addEventListener('install', (e) => {
-    self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => caches.delete(cacheName))
-            );
-        }).then(() => {
-            return self.clients.claim();
-        })
-    );
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-    // Pure bypass
+  // Pure bypass
 });
